aa29985dc45e63408315314252c3cbce619321db
Ricardo Godoi Kurashiki <62195354+RicardoKurashiki@users.noreply.github.com>
2023-10-29 09:32:27 -0300

    Inequality to equation & CLI to solver.py

---
+++ enums.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class SolverType(Enum):
+    MAXIMIZAR = 1
+    MINIMIZAR = 2
--- solver.py
+++ solver.py
@@ -1,22 +1,28 @@
 from enum import Enum
 
 # TODO: Ver se consegue pensar em outra lógica para lidar com os Ms, senão, usa isso mesmo.
+
+
 class BigNumber(Enum):
     M = 1000000000
 
 # Construção de string com M para mostrar iteração
+
+
 def buildMString(value):
     if abs(value / BigNumber.M.value) >= 0.1:
         tempString = f"{round(value / BigNumber.M.value,1)}M"
     else:
         tempString = f"{round(value, 4)}"
-        
+
     return tempString
 
 # Print na interface gráfica de cada iteração
+
+
 def printIteration(baseVars, matrix, bases):
     printString = f""
-    
+
     for i in range(len(baseVars)):
         # print(f"x{baseVars[i] + 1}      | {matrix[i]} | {bases[i]}")
         printString += f"x{baseVars[i] + 1} {'|' : >6} "
@@ -27,6 +33,8 @@ def printIteration(baseVars, matrix, bases):
     print(printString[:-1])
 
 # Print na interface gráfica de cada contribuição
+
+
 def printContributions(changeInfo, baseZj, artificials):
     if (len(artificials) <= 0):
         printString = "--------------------------------------------------------------\n"
@@ -50,32 +58,36 @@ def printContributions(changeInfo, baseZj, artificials):
             printString += f"{buildMString(changeInfo[1][i]) : <10} "
         printString += "\n--------------------------------------------------------------"
         print(printString)
-        
+
 # Calcula o Zj e o Cj - Zj, retornando uma matriz com os valores de ambos.
+
+
 def calcContribution(function, matrix, baseVars):
     result = list()
     numberOfElements = len(matrix)
     numberOfVars = len(function)
-    
+
     zj = [0] * (numberOfVars)
     cjmzj = [0] * (numberOfVars)
-    
+
     for i in range(numberOfVars):
         for j in range(numberOfElements):
             zj[i] += function[baseVars[j]] * matrix[j][i]
-    
+
     for i in range(numberOfVars):
         cjmzj[i] = function[i] - zj[i]
-        
+
     result.append(zj)
     result.append(cjmzj)
-    
+
     return result
 
 # Calcula Omega para cada variável de base.
+
+
 def calcOmega(changeInfo, matrix, bases):
-    omegaValues = [0] * len(bases) 
-    
+    omegaValues = [0] * len(bases)
+
     # Selecionando coluna pivô.
     pColumn = changeInfo[-1].index(max(changeInfo[-1]))
 
@@ -84,19 +96,23 @@ def calcOmega(changeInfo, matrix, bases):
             omegaValues[i] = bases[i]/matrix[i][pColumn]
         except:
             omegaValues[i] = -1
-    
+
     return omegaValues, pColumn
 
 # Calcula base do Zj
+
+
 def calcZjBase(func, baseVars, base):
     result = 0
-    
+
     for i in range(len(base)):
         result += func[baseVars[i]] * base[i]
-    
+
     return result
 
 # Realiza iteração alterando tudo
+
+
 def iterate(func, baseVars, matrix, bases, nIterations, bestSolution, artificials):
     # Matriz que armazena os valores de Zj (primeira linha) e Cj - Zj (segunda linha).
     changeInfo = calcContribution(func, matrix, baseVars)
@@ -104,13 +120,13 @@ def iterate(func, baseVars, matrix, bases, nIterations, bestSolution, artificial
     baseZj = calcZjBase(func, baseVars, bases)
 
     printContributions(changeInfo, baseZj, artificials)
-    
+
     # Trata-se de um problema de maximização, então quanto maior, melhor.
     if bestSolution[0] <= baseZj:
         bestSolution[0] = baseZj
         bestSolution[1] = baseVars
         bestSolution[2] = bases
-    
+
     if (max(changeInfo[-1]) <= 0):
         # Verificação de sistema degenerado
         if 0 in bestSolution[2]:
@@ -120,49 +136,50 @@ def iterate(func, baseVars, matrix, bases, nIterations, bestSolution, artificial
             for j in range(len(artificials)):
                 if baseVars[i] == artificials[j]:
                     print(">> Sistema Inviável <<")
-            
+
         return True
 
     # Calcula o Omega para cada variável de base e seleciona qual a coluna pivô.
     omegaInfo, pColumn = calcOmega(changeInfo, matrix, bases)
-    
+
     # Verificação de sistema sem fronteira
     for i in range(len(omegaInfo)):
         viableOmega = False
         if omegaInfo[i] >= 0:
             viableOmega = True
             break
-        
+
     if not viableOmega:
         print(">> Sistema sem fronteira <<")
         return True
-            
+
     # Verifica linha pivô - Menor valor positivo ou igual a zero.
-    pLine = omegaInfo.index(min([i for i in omegaInfo if i >= 0]))  
+    pLine = omegaInfo.index(min([i for i in omegaInfo if i >= 0]))
     # Armazena elemento pivô
     pElement = matrix[pLine][pColumn]
-    
+
     # Realiza troca das variáveis de base.
     baseVars[pLine] = pColumn
-    
+
     # Realiza as alterações na matriz para seguir troca de variáveis de base.
     for i in range(len(matrix)):
         if i != pLine:
             coef = matrix[i][pColumn]/pElement
-                
+
             for j in range(len(matrix[i])):
                 matrix[i][j] = matrix[i][j] - (coef * matrix[pLine][j])
-                
+
             bases[i] = bases[i] - (coef * bases[pLine])
-                
-    for i in range(len(matrix[pLine])): 
+
+    for i in range(len(matrix[pLine])):
         matrix[pLine][i] = matrix[pLine][i]/pElement
     bases[pLine] = bases[pLine]/pElement
-    
+
     print(f"{nIterations}° Iteracao")
     printIteration(baseVars, matrix, bases)
-    
+
     return False
+
 
 def solve(input_matrix):
     # bestSolution[0] = Melhor base de Zj
@@ -171,26 +188,29 @@ def solve(input_matrix):
     bestSolution = list()
     numberOfIterations = 1
     artificials = list()
-    
+
     # Extraindo valores da função
-    func = input_matrix[0][0 : (len(input_matrix[0]) - 1)]
+    func = input_matrix[0][0: (len(input_matrix[0]) - 1)]
     # Extraindo variáveis de base
-    baseVars = [i for i in range((len(input_matrix[0]) - len(input_matrix)), (len(input_matrix[0]) - 1), 1)]
+    baseVars = [i for i in range(
+        (len(input_matrix[0]) - len(input_matrix)), (len(input_matrix[0]) - 1), 1)]
     # Extraindo matriz
-    matrix = [input_matrix[i][0 : (len(input_matrix[0]) - 1)] for i in range(1, len(input_matrix))]
+    matrix = [input_matrix[i][0: (len(input_matrix[0]) - 1)]
+              for i in range(1, len(input_matrix))]
     # Extraindo bases
     bases = [input_matrix[i][-1] for i in range(1, len(input_matrix))]
-    
+
     # Extraindo variaveis artificiais, se houver
     # TODO: Esse método de estimativa de qtd de variáveis artificiais está estranho, rever isso aqui.
     numArt = len(func) - (2 * (len(matrix)))
     print(numArt)
     if numArt != 0:
-        artificials = [i for i in range((len(input_matrix[0]) - (numArt + 1)), (len(input_matrix[0]) - 1), 1)]
+        artificials = [i for i in range(
+            (len(input_matrix[0]) - (numArt + 1)), (len(input_matrix[0]) - 1), 1)]
         # Alterando coeficientes da função para valores tendendo ao infinito
         for i in range(len(artificials)):
             func[artificials[i]] = func[artificials[i]] * BigNumber.M.value
-    
+
     # Populando inicialmente a estrutura bestSolutions
     bestSolution.append(0)
     bestSolution.append(baseVars)
@@ -199,15 +219,15 @@ def solve(input_matrix):
     # Iteração inicial
     print(f"{numberOfIterations}° Iteracao")
     printIteration(baseVars, matrix, bases)
-        
+
     numberOfIterations += 1
     while (not iterate(func, baseVars, matrix, bases, numberOfIterations, bestSolution, artificials)):
         numberOfIterations += 1
-    
+
     print(f"Melhor resultado = {round(bestSolution[0], 4)}")
     for i in range(len(bestSolution[1])):
         print(f"x{bestSolution[1][i] + 1} = {round(bestSolution[2][i], 4)}")
-    
+
 # Exemplo:
 # Maximizar L: 6*x1 + 5*x2
 #        s.a.    x1 +   x2 <= 5
@@ -236,9 +256,9 @@ def solve(input_matrix):
 #                [3,2,0,1,12]]
 
 # Exemplo de sistema inviável
-input_matrix = [[4,3,0,0,-1,0],
-               [1,4,1,0,0,3],
-               [3,1,0,-1,1,12]]
+# input_matrix = [[4,3,0,0,-1,0],
+#                [1,4,1,0,0,3],
+#                [3,1,0,-1,1,12]]
 
 # Exemplo de minimização
 # input_matrix = [[-3,-4,0,0,-1,-1,0],
@@ -246,4 +266,4 @@ input_matrix = [[4,3,0,0,-1,0],
 #                [5,2,0,-1,0,1,12]]
 
 
-solve(input_matrix)
+# solve(input_matrix)
